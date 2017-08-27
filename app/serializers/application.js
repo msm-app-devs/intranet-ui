77d813016305ce1238e558d339477b935ba256cc
MVetov <m.vetov@gmail.com>
2017-08-27 22:03:14 +0300

    Update app serializator

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,26 +1,26 @@
-import Ember from 'ember';
+// import Ember from 'ember';
 import DS from 'ember-data';
 
 export default JSONAPISerializer.extend({
   keyForAttribute: function(attr) {
     return attr;
-  },
-
-  normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
-    let normalizedDocument = this._super(...arguments);
-
-    // Customize document meta
-    normalizedDocument.meta = camelCaseKeys(normalizedDocument.meta);
-
-    return normalizedDocument;
-  },
-
-  extractRelationship(relationshipHash) {
-    let normalizedRelationship = this._super(...arguments);
-
-    // Customize relationship meta
-    normalizedRelationship.meta = camelCaseKeys(normalizedRelationship.meta);
-
-    return normalizedRelationship;
   }
+
+  // normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
+  //   let normalizedDocument = this._super(...arguments);
+
+  //   // Customize document meta
+  //   normalizedDocument.meta = camelCaseKeys(normalizedDocument.meta);
+
+  //   return normalizedDocument;
+  // },
+
+  // extractRelationship(relationshipHash) {
+  //   let normalizedRelationship = this._super(...arguments);
+
+  //   // Customize relationship meta
+  //   normalizedRelationship.meta = camelCaseKeys(normalizedRelationship.meta);
+
+  //   return normalizedRelationship;
+  // }
 });
